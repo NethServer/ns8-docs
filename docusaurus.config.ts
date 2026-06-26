5fc4e819c9971e77bcc0686dad0a6dd2eae521d3
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2026-06-26 15:05:11 +0200

    feat: implement preview

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -27,7 +27,7 @@ const config: Config = {
     : 'https://nethserver.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: process.env.BASE_URL ?? '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
